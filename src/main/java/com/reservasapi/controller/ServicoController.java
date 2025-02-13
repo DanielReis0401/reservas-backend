20e7b8263bd0c5d7d9aba31528f479d0fa3f7d85
DanielReis0401 <daniel.reis.engineer@gmail.com>
2025-02-13 17:15:38 +0000

    Create ServicoController.java
    Implemented ServicoController

---
+++ src/main/java/com/reservasapi/controller/ServicoController.java
@@ -0,0 +1,46 @@
+package com.reservasapi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.reservasapi.model.servico.Servico;
+import com.reservasapi.service.ServicoService;
+
+@RestController
+@RequestMapping
+public class ServicoController {
+
+    @Autowired
+    private ServicoService servicoService;
+
+    @GetMapping("/reservation/{reservationId}")
+    public List<Servico> getServicosByReservation(@PathVariable Long reservationId){
+        return servicoService.getServicosByReservation(reservationId);
+    }
+
+    @PostMapping("/{reservationId}")
+    public ResponseEntity<Servico> addServico(@PathVariable Long reservationId, @RequestBody Servico servico){
+        return ResponseEntity.ok(servicoService.addServico(reservationId, servico));
+    }
+
+    @PutMapping("/{reservationId}/{servicoId}")
+    public ResponseEntity<Servico> updateServico(@PathVariable Long reservationId, @PathVariable Long servicoId, @RequestBody Servico updatedService){
+        return ResponseEntity.ok(servicoService.updateServico(reservationId, servicoId, updatedService));
+    }
+
+    @DeleteMapping("/{reservationId}/{serviceId}")
+    public ResponseEntity<Void> deleteServico(@PathVariable Long reservationId, @PathVariable Long servicoId){
+        servicoService.deleteServico(reservationId, servicoId);
+        return ResponseEntity.noContent().build();
+    }
+}
