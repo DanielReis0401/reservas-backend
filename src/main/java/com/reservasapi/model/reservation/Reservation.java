31ba5a6d75e20e7e1bf2e5a43d0c9b94d447425d
DanielReis0401 <daniel.reis.engineer@gmail.com>
2025-02-14 10:45:43 +0000

    Refactors total price fromm Reservation and adds some code refactors

--- src/main/java/com/reservasapi/model/reservation/Reservation.java
+++ src/main/java/com/reservasapi/model/reservation/Reservation.java
@@ -1,11 +1,7 @@
 package com.reservasapi.model.reservation;
 
-import java.time.LocalDate;
-import java.util.List;
-
 import com.reservasapi.model.passenger.Passenger;
 import com.reservasapi.model.servico.ReservationService;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -16,7 +12,15 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "reservations")
@@ -33,32 +37,70 @@ public class Reservation {
     @Column(name = "customer_name", nullable = false)
     private String customerName;
 
-    @Column(name = "reservation_date", nullable = false)
-    private LocalDate reservationDate;
+    @Column(name = "creation_date", nullable = false)
+    private LocalDate creationDate;
+
+    @Column(name = "total_price", nullable = false)
+    @Setter(AccessLevel.NONE)
+    private double totalPrice;
 
     @OneToMany(mappedBy = "reservation", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Passenger> passengers;
+    private List<Passenger> passengers = new ArrayList<>();
 
     @OneToMany(mappedBy = "reservation", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<ReservationService> reservationServices;
+    private List<ReservationService> services = new ArrayList<>();
 
-    @Column(name = "total_price")
-    private double totalPrice = 0.0;
+    /**
+     * Adds a passenger to the reservation.
+     *
+     * @param passenger the passenger to be added
+     */
+    public void addPassenger(Passenger passenger) {
+        passengers.add(passenger);
+    }
+
+    /**
+     * Removes a passenger from the reservation.
+     *
+     * @param passenger the passenger to be removed
+     */
+    public void removePassenger(Passenger passenger) {
+        passengers.remove(passenger);
+    }
+
+    /**
+     * Adds a service to the reservation.
+     *
+     * @param service the service to be added
+     */
+    public void addService(ReservationService service) {
+        services.add(service);
+    }
+
+    /**
+     * Removes a service from the reservation.
+     *
+     * @param service the service to be removed
+     */
+    public void removeService(ReservationService service) {
+        services.remove(service);
+    }
 
     /**
      * Calculates the total price of the reservation based on the services and the number of passengers.
      */
-    public void calculateTotalPrice() {
-        if (reservationServices == null || passengers == null)
-            return;
-
-        this.totalPrice = reservationServices.stream().mapToDouble(ReservationService::getPrice).sum() // Sum of the prices of all services
+    public double getTotalPrice() {
+        return services.stream().mapToDouble(ReservationService::getPrice).sum()
                 * passengers.size();
     }
 
+    /**
+     * Updates the total price of the reservation before saving it.
+     */
     @PrePersist
     @PreUpdate
-    private void preSave() {
-        calculateTotalPrice();
+    private void setTotalPrice() {
+        this.totalPrice = getTotalPrice();
     }
+
 }
--- src/main/java/com/reservasapi/service/ReservationService.java
+++ src/main/java/com/reservasapi/service/ReservationService.java
@@ -1,48 +1,44 @@
 package com.reservasapi.service;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.reservasapi.model.reservation.Reservation;
+import com.reservasapi.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.reservasapi.model.reservation.Reservation;
-import com.reservasapi.repository.ReservationRepository;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReservationService {
 
     private final ReservationRepository reservationRepository;
-    
+
     @Autowired
-    public ReservationService(ReservationRepository reservationRepository){
+    public ReservationService(ReservationRepository reservationRepository) {
         this.reservationRepository = reservationRepository;
     }
 
     //Criar uma nova reserva
-    public Reservation createReservation(Reservation reservation){
+    public Reservation createReservation(Reservation reservation) {
         return reservationRepository.save(reservation);
     }
 
     //Procurar uma reserva por ID
-    public Optional<Reservation> getReservationById(Long id){
+    public Optional<Reservation> getReservationById(Long id) {
         return reservationRepository.findById(id);
     }
 
     //Atualizar uma reserva existente
-    public Reservation updateReservation(Long id, Reservation updatedReservation){
+    public Reservation updateReservation(Long id, Reservation updatedReservation) {
         Optional<Reservation> existingReservation = reservationRepository.findById(id);
 
-        if(existingReservation.isPresent()){
+        if (existingReservation.isPresent()) {
             //Copiar os valores da updatedReservation para a reserva existente
             Reservation reservation = existingReservation.get();
             reservation.setCustomerName(updatedReservation.getCustomerName());
-            reservation.setReservationDate(updatedReservation.getReservationDate());
+            reservation.setCreationDate(updatedReservation.getCreationDate());
             reservation.setPassengers(updatedReservation.getPassengers());
-            reservation.setReservationServices(updatedReservation.getReservationServices());
-
-            //Recalcular o preco total
-            reservation.calculateTotalPrice();
+            reservation.setServices(updatedReservation.getServices());
 
             return reservationRepository.save(reservation);
         } else {
@@ -51,9 +47,9 @@ public class ReservationService {
     }
 
     //Apagar uma reserva
-    public void deleteReservation(Long id){
+    public void deleteReservation(Long id) {
         Optional<Reservation> existingReservation = reservationRepository.findById(id);
-        if(existingReservation.isPresent()){
+        if (existingReservation.isPresent()) {
             reservationRepository.delete(existingReservation.get());
         } else {
             throw new RuntimeException("Reservation not found with ID" + id);
@@ -61,19 +57,14 @@ public class ReservationService {
     }
 
     //Listar todas as reservas
-    public List<Reservation> getAllReservations(){
+    public List<Reservation> getAllReservations() {
         return reservationRepository.findAll();
     }
 
     //Encontrar reservas atraves do nome do cliente
-    public List<Reservation> getReservationsByClientName(String customerName){
+    public List<Reservation> getReservationsByClientName(String customerName) {
         return reservationRepository.findByCostumerName(customerName);
-    } 
-
-    //Calcular preco total de uma reserva
-    public double calculateTotalPrice(Reservation reservation){
-        reservation.calculateTotalPrice();
-        return reservation.getTotalPrice();
     }
+
 
 }
