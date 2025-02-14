088886ad3315399602005713901803713fc6117f
DanielReis0401 <daniel.reis.engineer@gmail.com>
2025-02-14 01:08:51 +0000

    Adds Swagger

---
+++ src/main/java/com/reservasapi/reservas_backend/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.reservasapi.reservas_backend.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Reservas API")
+                        .version("1.0")
+                        .description("API para gestão de reservas e passageiros")
+                );
+    }
+}
