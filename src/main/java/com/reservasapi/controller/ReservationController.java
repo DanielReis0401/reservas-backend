ca81c844f0bfb44815c144425863ebfd94f39b09
DanielReis0401 <daniel.reis.engineer@gmail.com>
2025-02-14 20:03:54 +0000

    Adds swagger to ReservationController

--- src/main/java/com/reservasapi/controller/ReservationController.java
+++ src/main/java/com/reservasapi/controller/ReservationController.java
@@ -4,6 +4,10 @@ import com.reservasapi.model.reservation.Reservation;
 import com.reservasapi.service.ReservationService;
 import java.util.List;
 import java.util.Optional;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,17 +22,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/reservations")
+@Tag(name = "Reservations", description = "Endpoints for managing reservations")
 public class ReservationController {
 
   @Autowired
   private ReservationService reservationService;
 
   @GetMapping
+  @Operation(summary = "Get all reservations", description = "Fetches all the reservations from the system")
   public List<Reservation> getAllReservations() {
     return reservationService.getAllReservations();
   }
 
   @GetMapping("/{id}")
+  @Operation( summary = "Get a reservation by ID", description = "Fetches a reservation by its unique identifier")
   public ResponseEntity<Optional<Reservation>> getReservationById(
     @PathVariable Long id
   ) {
@@ -36,6 +43,10 @@ public class ReservationController {
   }
 
   @PostMapping
+  @Operation(
+          summary = "Create a new reservation",
+          description = "Creates a new reservation based on the provided details"
+  )
   public ResponseEntity<Reservation> createReservation(
     @RequestBody Reservation reservation
   ) {
@@ -43,6 +54,10 @@ public class ReservationController {
   }
 
   @PutMapping("/{id}")
+  @Operation(
+          summary = "Update an existing reservation",
+          description = "Updates an existing reservation identified by the ID"
+  )
   public ResponseEntity<Reservation> updateReservation(
     @PathVariable Long id,
     @RequestBody Reservation updatedReservation
@@ -52,12 +67,21 @@ public class ReservationController {
     );
   }
 
+
   @DeleteMapping("/{id}")
+  @Operation(
+          summary = "Delete a reservation",
+          description = "Deletes the reservation identified by the provided ID"
+  )
   public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
     reservationService.deleteReservation(id);
     return ResponseEntity.noContent().build();
   }
 
+  @Operation(
+          summary = "Search reservations by client name",
+          description = "Fetches all reservations for a specific client based on the client name"
+  )
   @GetMapping("/search")
   public List<Reservation> searchByClientName(@RequestParam String clientName) {
     return reservationService.getReservationsByClientName(clientName);
