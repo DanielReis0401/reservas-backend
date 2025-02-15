6f21f29cf7bebd0cbb2b274746458f6340f0377c
DanielReis0401 <daniel.reis.engineer@gmail.com>
2025-02-15 01:55:42 +0000

    Adds Id to the Passenger mapping

--- src/main/java/com/reservasapi/model/mapper/BaseMapper.java
+++ src/main/java/com/reservasapi/model/mapper/BaseMapper.java
@@ -6,7 +6,9 @@ import com.reservasapi.dto.ReservationServiceDTO;
 import com.reservasapi.model.passenger.Passenger;
 import com.reservasapi.model.reservation.Reservation;
 import com.reservasapi.model.service.ReservationService;
+
 import java.util.List;
+
 import org.mapstruct.Named;
 
 public interface BaseMapper {
@@ -20,98 +22,96 @@ public interface BaseMapper {
     @Named(PASSENGER_DTO)
     default List<PassengerDTO> toPassengerDTO(List<Passenger> passengers) {
         return passengers
-            .stream()
-            .map(passengerDto ->
-                PassengerDTO.builder()
-                    .name(passengerDto.getName())
-                    .age(passengerDto.getAge())
-                    .type(passengerDto.getType())
-                    .build()
-            )
-            .toList();
+                .stream()
+                .map(passenger ->
+                        PassengerDTO.builder()
+                                .id(passenger.getId())
+                                .name(passenger.getName())
+                                .age(passenger.getAge())
+                                .type(passenger.getType())
+                                .build()
+                )
+                .toList();
     }
 
     @Named(PASSENGER)
     default List<Passenger> toPassenger(List<PassengerDTO> passengerDTOs) {
         return passengerDTOs
-            .stream()
-            .map(passengerDto ->
-                Passenger.builder()
-                    .name(passengerDto.getName())
-                    .age(passengerDto.getAge())
-                    .type(passengerDto.getType())
-                    .build()
-            )
-            .toList();
+                .stream()
+                .map(passengerDto ->
+                        Passenger.builder()
+                                .id(passengerDto.getId())
+                                .name(passengerDto.getName())
+                                .age(passengerDto.getAge())
+                                .type(passengerDto.getType())
+                                .build()
+                )
+                .toList();
     }
 
     @Named(RES_SERVICE_DTO)
     default List<ReservationServiceDTO> toReservationServiceDTO(
-        List<ReservationService> reservationServices
+            List<ReservationService> reservationServices
     ) {
         return reservationServices
-            .stream()
-            .map(resServiceDto ->
-                ReservationServiceDTO.builder()
-                    .name(resServiceDto.getName())
-                    .description(resServiceDto.getDescription())
-                    .price(resServiceDto.getPrice())
-                    .build()
-            )
-            .toList();
+                .stream()
+                .map(resService ->
+                        ReservationServiceDTO.builder()
+                                .name(resService.getName())
+                                .description(resService.getDescription())
+                                .price(resService.getPrice())
+                                .build()
+                )
+                .toList();
     }
 
     @Named(RES_SERVICE)
     default List<ReservationService> toReservationService(
-        List<ReservationServiceDTO> reservationServiceDTOs
+            List<ReservationServiceDTO> reservationServiceDTOs
     ) {
         return reservationServiceDTOs
-            .stream()
-            .map(resServiceDto ->
-                ReservationService.builder()
-                    .name(resServiceDto.getName())
-                    .description(resServiceDto.getDescription())
-                    .price(resServiceDto.getPrice())
-                    .build()
-            )
-            .toList();
+                .stream()
+                .map(resServiceDto ->
+                        ReservationService.builder()
+                                .name(resServiceDto.getName())
+                                .description(resServiceDto.getDescription())
+                                .price(resServiceDto.getPrice())
+                                .build()
+                )
+                .toList();
     }
 
     @Named(RESERVATION_DTO)
     default List<ReservationDTO> toReservationDTO(
-        List<Reservation> reservations
+            List<Reservation> reservations
     ) {
         return reservations
-            .stream()
-            .map(reservationDto ->
-                ReservationDTO.builder()
-                    .customerName(reservationDto.getCustomerName())
-                    .creationDate(reservationDto.getCreationDate())
-                    .services(
-                        toReservationServiceDTO(reservationDto.getServices())
-                    )
-                    .passengers(toPassengerDTO(reservationDto.getPassengers()))
-                    .build()
-            )
-            .toList();
+                .stream()
+                .map(reservation ->
+                        ReservationDTO.builder()
+                                .customerName(reservation.getCustomerName())
+                                .creationDate(reservation.getCreationDate())
+                                .services(toReservationServiceDTO(reservation.getServices()))
+                                .passengers(toPassengerDTO(reservation.getPassengers()))
+                                .build()
+                )
+                .toList();
     }
 
     @Named(RESERVATION)
     default List<Reservation> toReservation(
-        List<ReservationDTO> reservationDTOs
+            List<ReservationDTO> reservationDTOs
     ) {
         return reservationDTOs
-            .stream()
-            .map(reservationDto ->
-                Reservation.builder()
-                    .customerName(reservationDto.getCustomerName())
-                    .creationDate(reservationDto.getCreationDate())
-                    .services(
-                        toReservationService(reservationDto.getServices())
-                    )
-                    .passengers(toPassenger(reservationDto.getPassengers()))
-                    .build()
-            )
-            .toList();
+                .stream()
+                .map(reservationDto ->
+                        Reservation.builder()
+                                .customerName(reservationDto.getCustomerName())
+                                .creationDate(reservationDto.getCreationDate())
+                                .services(toReservationService(reservationDto.getServices()))
+                                .passengers(toPassenger(reservationDto.getPassengers()))
+                                .build()
+                )
+                .toList();
     }
 }
