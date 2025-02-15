7b99013783bd384ace87371bae90c33df9d72c11
DanielReis0401 <daniel.reis.engineer@gmail.com>
2025-02-15 00:46:29 +0000

    Adds ReservationMapper

--- src/main/java/com/reservasapi/service/ReservationService.java
+++ src/main/java/com/reservasapi/service/ReservationService.java
@@ -1,14 +1,22 @@
 package com.reservasapi.service;
 
+import com.reservasapi.dto.ReservationDTO;
+import com.reservasapi.model.mapper.PassengerMapper;
+import com.reservasapi.model.mapper.ReservationMapper;
 import com.reservasapi.model.reservation.Reservation;
 import com.reservasapi.repository.ReservationRepository;
 import java.util.List;
 import java.util.Optional;
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReservationService {
+
+  private static final ReservationMapper MAPPER = Mappers.getMapper(
+    ReservationMapper.class
+  );
 
   private final ReservationRepository reservationRepository;
 
@@ -18,8 +26,8 @@ public class ReservationService {
   }
 
   //Criar uma nova reserva
-  public Reservation createReservation(Reservation reservation) {
-    return reservationRepository.save(reservation);
+  public Reservation createReservation(ReservationDTO reservationDTO) {
+    return reservationRepository.save(MAPPER.toReservation(reservationDTO));
   }
 
   //Procurar uma reserva por ID
@@ -30,7 +38,7 @@ public class ReservationService {
   //Atualizar uma reserva existente
   public Reservation updateReservation(
     Long id,
-    Reservation updatedReservation
+    ReservationDTO updatedReservationDTO
   ) {
     Optional<Reservation> existingReservation = reservationRepository.findById(
       id
@@ -39,10 +47,18 @@ public class ReservationService {
     if (existingReservation.isPresent()) {
       //Copiar os valores da updatedReservation para a reserva existente
       Reservation reservation = existingReservation.get();
-      reservation.setCustomerName(updatedReservation.getCustomerName());
-      reservation.setCreationDate(updatedReservation.getCreationDate());
-      reservation.setPassengers(updatedReservation.getPassengers());
-      reservation.setServices(updatedReservation.getServices());
+      reservation.setCustomerName(
+        MAPPER.toReservation(updatedReservationDTO).getCustomerName()
+      );
+      reservation.setCreationDate(
+        MAPPER.toReservation(updatedReservationDTO).getCreationDate()
+      );
+      reservation.setPassengers(
+        MAPPER.toReservation(updatedReservationDTO).getPassengers()
+      );
+      reservation.setServices(
+        MAPPER.toReservation(updatedReservationDTO).getServices()
+      );
 
       return reservationRepository.save(reservation);
     } else {
